176f52bde5ee1cfbbd1bf94c9fcfe75888b941ad
Alok RG <alokratan277@gmail.com>
2022-07-07 21:38:00 +0530

    finl

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+<div className="navbar">
+<div className="logo">
+<p>HF</p>
+</div>
+<div className="menu">
+  <ul>
+    <li>
+      <a href='/'><i className="bi bi-camera-reels-fill"></i>LIVE</a></li>
+    <li><a href='/'><i className="bi bi-postcard-fill"></i>MEMBERSHIP</a></li>
+    <li><a href='/'><i className="bi bi-clipboard-data-fill"></i>PLANS</a></li>
+    <li><a href='/'><i className="bi bi-grid-fill"></i>MORE</a></li>
+  </ul>
+</div>
+<div className="login"><p><i className="bi bi-person-fill"></i>Login</p></div>
+</div>
+<div className="workout">
+</div>
+
+<div className="containers">
+<h1>
+  Runner's Workout
+</h1>
+<div className="slider">
+<div className="strength abc"><h5>Strength</h5><i className="bi bi-arrow-right-circle-fill"></i></div>
+<div className="mobility abc"><h5>Mobility</h5><i className="bi bi-arrow-right-circle-fill"></i></div>
+<div className="drills abc"><h5>Drills</h5><i className="bi bi-arrow-right-circle-fill"></i></div>
+</div>
+</div>
+
+
+
+    </>
   );
 }
 
